353924a49bca6fdeeed4536d2302821450d742eb
Lari <88565631+romugaboo@users.noreply.github.com>
2025-01-27 17:58:06 -0300

    Configuração prettier e ESLint

--- event-front-end/src/pages/page.tsx
+++ event-front-end/src/pages/page.tsx
@@ -1,6 +1,9 @@
+import Header from "../components/header";
+
 export default function Home() {
   return (
     <main>
+      <Header/>
       <h1 className="text-3xl font-bold text-center p-5">
           Venda de Ingressos
       </h1>
