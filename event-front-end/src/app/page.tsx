dcf45963c81917766b620462f0b57c43495b45c5
Lari <88565631+romugaboo@users.noreply.github.com>
2025-01-27 20:52:35 -0300

    Add grid de shows na página principal

--- event-front-end/src/app/page.tsx
+++ event-front-end/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import EventItem from '../components/event-item';
 
 export default function Home() {
   return (
@@ -9,6 +10,51 @@ export default function Home() {
           src="/images/gradient-banner.jpg"
           alt="Gradient Banner"
           layout="fill"
+        />
+      </div>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-16 px-40'>
+        <EventItem
+          avatar='/images/avatar.png'
+          banner='/images/concert-banner.png'
+          title="Festa Maneira"
+          local="Longe - Acre"
+          date='27 jan. de 2024'
+        />
+        <EventItem
+          avatar='/images/avatar.png'
+          banner='/images/concert-banner.png'
+          title="Festa Maneira"
+          local="Longe - Acre"
+          date='27 jan. de 2024'
+        />
+        <EventItem
+          avatar='/images/avatar.png'
+          banner='/images/concert-banner.png'
+          title="Festa Maneira"
+          local="Longe - Acre"
+          date='27 jan. de 2024'
+        />
+        <EventItem
+          avatar='/images/avatar.png'
+          banner='/images/concert-banner.png'
+          title="Festa Maneira"
+          local="Longe - Acre"
+          date='27 jan. de 2024'
+        />
+        <EventItem
+          avatar='/images/avatar.png'
+          banner='/images/concert-banner.png'
+          title="Festa Maneira"
+          local="Longe - Acre"
+          date='27 jan. de 2024'
+        />
+        <EventItem
+          avatar='/images/avatar.png'
+          banner='/images/concert-banner.png'
+          title="Festa Maneira"
+          local="Longe - Acre"
+          date='27 jan. de 2024'
         />
       </div>
     </main>
