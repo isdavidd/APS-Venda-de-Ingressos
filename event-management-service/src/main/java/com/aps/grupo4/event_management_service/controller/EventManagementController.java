af79f27e4d0a269dd9b091069822cdca8d5c08ea
isdavidd <vini1100@outlook.com>
2025-01-30 00:03:34 -0300

    Novo mapeamento de entidade de acordo com o conteiner (essa porra vai mudar em breve dnv)

--- event-management-service/src/main/java/com/aps/grupo4/event_management_service/controller/EventManagementController.java
+++ event-management-service/src/main/java/com/aps/grupo4/event_management_service/controller/EventManagementController.java
@@ -4,6 +4,7 @@ package com.aps.grupo4.event_management_service.controller;
 import com.aps.grupo4.event_management_service.config.validations.exceptions.EventoExistenteException;
 import com.aps.grupo4.event_management_service.config.validations.exceptions.EventoInexistenteException;
 import com.aps.grupo4.event_management_service.entity.Evento;
+import com.aps.grupo4.event_management_service.entity.converter.UFEnum;
 import com.aps.grupo4.event_management_service.service.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -35,7 +36,7 @@ public class EventManagementController {
 
 
     @GetMapping(path = "/events/id-evento/{id}")
-    public ResponseEntity<Object> buscaEventoPorId(@PathVariable("id") Integer id) {
+    public ResponseEntity<Object> buscaEventoPorId(@PathVariable("id") Long id) {
         try {
             var evento = eventoService.getEventoById(id);
 
@@ -64,6 +65,55 @@ public class EventManagementController {
         }
     }
 
+    @GetMapping(path = "/events/capacidade-minima-evento/{capacidade-minima}")
+    public ResponseEntity<Object> buscaEventoPorCapacidadeMinima(@PathVariable("capacidade-minima") Integer capacidadeMinima) {
+        try {
+            var eventos = eventoService.getEventoByCapacidadeMinima(capacidadeMinima);
+
+            if (eventos.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+            }
+
+            return ResponseEntity.ok().body(eventos);
+
+        } catch (RuntimeException e) {
+            return ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, null);
+        }
+    }
+
+    @GetMapping(path = "/events/capacidade-maxima-evento/{capacidade-maxima}")
+    public ResponseEntity<Object> buscaEventoPorCapacidade(@PathVariable("capacidade-maxima") Integer capacidadeMaxima) {
+        try {
+            var eventos = eventoService.getEventoByCapacidadeMaxima(capacidadeMaxima);
+
+            if (eventos.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+            }
+
+            return ResponseEntity.ok().body(eventos);
+
+        } catch (RuntimeException e) {
+            return ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, null);
+        }
+    }
+
+    @GetMapping(path = "/events/uf-evento/{uf}")
+    public ResponseEntity<Object> buscaEventosPorSiglaUF(@PathVariable("uf") String uf) {
+        try {
+            var eventos = eventoService.getEventosByUF(UFEnum.valueOf(uf.toUpperCase()));
+
+            if (eventos.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+            }
+
+            return ResponseEntity.ok().body(eventos);
+
+        } catch (RuntimeException e) {
+            return ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, null);
+        }
+    }
+
+
     @PostMapping(path = "/create-event")
     public ResponseEntity<Object> createEvento(@RequestBody Evento eventoNovo) {
         try {
@@ -72,10 +122,10 @@ public class EventManagementController {
             return ResponseEntity.ok().body(evento);
 
         } catch (EventoExistenteException e) {
-            return  ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.BAD_REQUEST, null);
+            return ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.BAD_REQUEST, null);
 
         } catch (RuntimeException e) {
-            return  ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, null);
+            return ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, null);
         }
     }
 
@@ -87,15 +137,15 @@ public class EventManagementController {
             return ResponseEntity.ok().body(evento);
 
         } catch (EventoInexistenteException e) {
-            return  ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.NO_CONTENT, null);
+            return ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.NO_CONTENT, null);
 
         } catch (RuntimeException e) {
-            return  ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, null);
+            return ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, null);
         }
     }
 
     @DeleteMapping(path = "/delete-event/{id}")
-    public ResponseEntity<Object> deleteEvento(@PathVariable("id") Integer idEvento) {
+    public ResponseEntity<Object> deleteEvento(@PathVariable("id") Long idEvento) {
         try {
             var eventoDeletado = eventoService.deleteEvento(idEvento);
 
@@ -105,7 +155,7 @@ public class EventManagementController {
             return ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.NO_CONTENT, null);
 
         } catch (RuntimeException e) {
-            return  ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, null);
+            return ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, null);
         }
     }
 }
