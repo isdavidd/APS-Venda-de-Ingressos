90269b4f40e751fd0be7203b4b4ce52b1325ec02
Daniel Pedro <danielpedro7676@gmail.com>
2025-02-03 10:34:29 -0300

    Modificando endpoint que retorna basic auth do usuário

--- services/user-service/src/main/java/com/aps/grupo4/user_service/controller/AuthController.java
+++ services/user-service/src/main/java/com/aps/grupo4/user_service/controller/AuthController.java
@@ -1,13 +1,11 @@
 package com.aps.grupo4.user_service.controller;
 
+import com.aps.grupo4.user_service.entity.dto.UsuarioLoginRequest;
 import com.aps.grupo4.user_service.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Base64;
 
@@ -19,15 +17,15 @@ public class AuthController {
     private UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<String> generateBasicToken(@RequestParam String cpf, @RequestParam String senha) {
+    public ResponseEntity<String> generateBasicToken(@RequestBody UsuarioLoginRequest request) {
 
-        var usuario = usuarioService.getUsuarioPorCpf(cpf);
+        var usuario = usuarioService.getUsuarioPorCpf(request.getCpf());
 
         if (usuario == null) {
             return ResponseEntity.status(HttpStatusCode.valueOf(403)).build();
         }
 
-        String auth = cpf + ":" + senha;
+        String auth = request.getCpf() + ":" + request.getSenha();
         String basicToken = "Basic " + Base64.getEncoder().encodeToString(auth.getBytes());
 
         return ResponseEntity.ok(basicToken);
---
+++ services/user-service/src/main/java/com/aps/grupo4/user_service/entity/dto/UsuarioLoginRequest.java
@@ -0,0 +1,10 @@
+package com.aps.grupo4.user_service.entity.dto;
+
+public class UsuarioLoginRequest {
+    private String cpf;
+    private String senha;
+
+    public String getCpf() { return cpf; }
+    public String getSenha() { return senha; }
+}
+
