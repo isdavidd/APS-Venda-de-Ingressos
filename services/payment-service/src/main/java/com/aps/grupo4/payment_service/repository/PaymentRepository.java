697e68d6bc8f6d7b6f747384b2d7c710ae80c3c8
Daniel Pedro <danielpedro7676@gmail.com>
2025-02-03 01:45:44 -0300

    [WIP] Subindo todos os serviços com o docker compose

--- services/payment-service/src/main/java/com/aps/grupo4/payment_service/repository/PaymentRepository.java
+++ services/payment-service/src/main/java/com/aps/grupo4/payment_service/repository/PaymentRepository.java
@@ -5,9 +5,11 @@ import com.aps.grupo4.payment_service.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     @Query(value = "SELECT * FROM pagamento p WHERE p.id_usuario = :usuarioId", nativeQuery = true)
     List<Payment> findPaymentsByUsuarioId(@Param("usuarioId") Long usuarioId);
