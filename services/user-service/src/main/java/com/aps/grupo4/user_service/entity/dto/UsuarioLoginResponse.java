1fbde4b2862d1bb896c08ab1302fecaab6191bc6
isdavidd <vini1100@outlook.com>
2025-02-05 23:00:23 -0300

    Retornando mais informações ao autenticar

--- services/user-service/src/main/java/com/aps/grupo4/user_service/entity/dto/UsuarioLoginResponse.java
+++ services/user-service/src/main/java/com/aps/grupo4/user_service/entity/dto/UsuarioLoginResponse.java
@@ -7,20 +7,28 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public class UsuarioLoginResponse {
     @Schema(example = "nome legal")
     private String nome;
+    @Schema(example = "19607112709")
+    private String cpf;
     @Schema(example = "teste@gmail.com")
     private String email;
+    @Schema(example = "21986607486")
+    private String telefone;
     @Schema(example = "ADMIN")
     private String role;
     @Schema(example = "Basic MTk2MDcxMTI3MDk6dGVzdGUxMjM=")
     private String token;
 
     public String getNome() { return nome; }
+    public String getCpf() { return cpf; }
     public String getEmail() { return email; }
+    public String getTelefone() { return telefone; }
     public String getRole() { return role; }
     public String getToken() { return token; }
 
     public void setNome(String nome) { this.nome = nome; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
     public void setEmail(String email) { this.email = email; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
     public void setRole(String role) { this.role = role; }
     public void setToken(String token) { this.token = token; }
 }
